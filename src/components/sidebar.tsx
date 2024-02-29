2dfe491dc68feb52b24dae40dda60345a69c17b5
Pedro Galembeck <galembeckpedro@gmail.com>
2024-02-29 20:30:17 -0300

    refactor: update sidebar import functions

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { Home, Plus, Settings } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
+import { Home, Plus, Settings } from "lucide-react";
 
 export function Sidebar() {
   const pathname = usePathname();
