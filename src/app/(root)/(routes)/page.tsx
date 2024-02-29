0de7524996b27119941d401cfb05438ef4b4d6a7
Pedro Galembeck <galembeckpedro@gmail.com>
2024-02-29 19:36:14 -0300

    refactor: add "UserButton" clerk component

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function RootPage() {
   return (
     <div>
-      Root Page (Protected)
+      <UserButton afterSignOutUrl="/sign-in" />
     </div>
   );
 }
