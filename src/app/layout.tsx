297b7cabdb1451739e02065085089152177d4643
Pedro Galembeck <galembeckpedro@gmail.com>
2024-02-29 20:29:19 -0300

    refactor: correct import functions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 import { ClerkProvider } from '@clerk/nextjs'
+
 import { ThemeProvider } from "@/components/theme-provider";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
